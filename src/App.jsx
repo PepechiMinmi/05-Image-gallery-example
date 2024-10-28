903fb52b855abdc5c1391856dc1d696c3e800ada
PepechiMinmi <p5420077@stu.chs.nihon-u.ac.jp>
2024-10-28 19:03:48 +0900

    Reactチュートリアル3:データのマッピング

---
+++ src/App.jsx
@@ -0,0 +1,63 @@
+import { useState } from "react";
+
+export default function App() {
+
+    //画像配列
+    const images=['pic1.jpg','pic2.jpg','pic3.jpg','pic4.jpg','pic5.jpg'];
+
+    //alt配列
+    const alts={
+        'pic1.jpg' : 'Closeup of a human eye',
+        'pic2.jpg' : 'Rock that looks like a wave',
+        'pic3.jpg' : 'Purple and white pansies',
+        'pic4.jpg' : 'Section of wall from a pharoah\'s tomb',
+        'pic5.jpg' : 'Large moth on a leaf'
+    };
+
+    //5-3:サムネイルクリックイベント
+    const [displayedImage,setDispalyedImage]=useState('pic1.jpg');
+    //サムネイルハンドラ
+    const handleThunmbBarCick=(image)=>{
+        setDispalyedImage(image);
+    };
+
+    //5-4:画像を暗くするイベントbooleanで
+    const [isDarken,setIsDarken]=useState('false');
+
+    const handleDarkenClick=()=>{
+        setIsDarken(!isDarken);
+    };
+
+    return (
+      <>
+        <h1>Image gallery example</h1>
+        <div className="full-img">
+          <img
+            className="displayed-img"
+            src={`images/${displayedImage}`}//変更
+            alt={alts[displayedImage]}//変更
+          />
+          <div 
+            className="overlay"
+            style={{//追加
+                backgroundColor:isDarken ? 'rgba(0,0,0,0.5)': 'rgba(0,0,0,0)',
+            }}
+          ></div>
+          <button className="dark" onClick={handleDarkenClick}>
+            {isDarken ? 'Lighten':'Darken'}
+          </button>
+        </div>
+        <div className="thumb-bar">
+            {images.map((image)=>(
+                <img
+                    key={image}
+                    src={`images/${image}`}
+                    alt={alts[image]}
+                    className="thumb-img"
+                    onClick={()=>handleThunmbBarCick(image)}//サムネイルハンドラ
+                />
+            ))}
+        </div>
+      </>
+    );
+  }
